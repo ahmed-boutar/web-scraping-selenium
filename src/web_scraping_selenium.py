f608c34af558881596d90b47b905e4a724ee05bb
Tal <61614571+notthattal@users.noreply.github.com>
2024-08-28 17:34:39 -0400

    Removed test website and added an option to run Chrome in headless mode

--- src/web_scraping_selenium.py
+++ src/web_scraping_selenium.py
@@ -1,15 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
 # Download the correct ChromeDriver
 chromeDriver = ChromeDriverManager().install()
 
-# Start Chrome
-driver = webdriver.Chrome(service=Service(chromeDriver))
+# Create an options for specifications on how to run the driver
+chromeOptions = Options()
 
-# Open the website to be scraped 
-driver.get('https://hoopshype.com/salaries/players/')
+# Add an option for Chrome to run in the background instead of opening a physical window
+chromeOptions.add_argument("--headless=new")
+
+# Start Chrome
+driver = webdriver.Chrome(service=Service(chromeDriver), options=chromeOptions)
     
 # Shut the driver down
-driver.close()
+driver.quit()
